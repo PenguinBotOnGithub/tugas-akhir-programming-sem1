0945e6a6240fde8f6f5d7785ab055db262b47c8c
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2025-12-05 22:13:36 +0700

    feat: add main menu placeholder

---
+++ src/menus/main_menu.cpp
@@ -0,0 +1,6 @@
+#include "menus.hpp"
+#include <cstdio>
+
+void main_menu_loop() {
+    printf("FIXME: main menu not implemented yet\n");
+}
--- src/menus/menus.hpp
+++ src/menus/menus.hpp
@@ -1,4 +1,5 @@
 #ifndef _MENUS_
 #define _MENUS_
 void start_menu_loop();
+void main_menu_loop();
 #endif
