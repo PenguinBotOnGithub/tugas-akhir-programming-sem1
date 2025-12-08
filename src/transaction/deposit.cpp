11241ae886a52a00ee7e1d4c6f05a75dd0f291a7
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2025-12-08 11:41:22 +0700

    feat: add deposit and withdraw feature (Fasik)

---
+++ src/transaction/deposit.cpp
@@ -0,0 +1,28 @@
+#include <cstdint>
+#include <iostream>
+#include "transaction.hpp"
+#include "../globals.hpp"
+
+void deposit() {
+    int64_t amount;
+
+    cout << "\n=== Deposit Money ===\n";
+    cout << "Enter amount: ";
+    cin >> amount;
+
+    if (amount <= 0) {
+        cout << "Invalid amount!\n";
+        return;
+    }
+
+    logged_in_account->balance += amount;
+
+    TransactionRecord tr;
+    tr.id_sender = -1; // bank
+    tr.id_receiver = (int64_t)(logged_in_account - &accounts[0]);
+    tr.transaction_type = DEPOSIT;
+    tr.amount = amount;
+    transaction_records.push_back(tr);
+
+    cout << "Deposit successful!\n";
+}
---
+++ src/transaction/withdraw.cpp
@@ -0,0 +1,30 @@
+#include <cstdint>
+#include <iostream>
+#include "transaction.hpp"
+#include "../globals.hpp"
+
+using namespace std;
+
+void withdraw_money() {
+    int64_t amount;
+
+    cout << "\n=== Withdraw Money ===\n";
+    cout << "Enter amount: ";
+    cin >> amount;
+
+    if (amount <= 0 || amount > logged_in_account->balance) {
+        cout << "Invalid or insufficient balance!\n";
+        return;
+    }
+
+    logged_in_account->balance -= amount;
+
+    TransactionRecord tr;
+    tr.id_sender = (int64_t)(logged_in_account - &accounts[0]);
+    tr.id_receiver = -1; // bank
+    tr.transaction_type = WITHDRAW;
+    tr.amount = amount;
+    transaction_records.push_back(tr);
+
+    cout << "Withdraw successful!\n";
+}
